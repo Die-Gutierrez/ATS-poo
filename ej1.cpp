e4fb51e83be78fc8c1469dfe67bd28ee774cc344
Die-Gutierrez <dgutierrez@unjbg.edu.pe>
2022-10-14 17:11:55 -0500

    Atributos y Metodos de la clase Rectangulo

---
+++ ej1.cpp
@@ -0,0 +1,25 @@
+/*Construya una clase llamda Rectangulo que tenga los siguientes atributos: largo y ancho,
+y los siguientes metodos: perimetro() y area()*/
+#include<iostream>
+
+using namespace std;
+
+class Rectangulo
+{
+    private: // Atributos de la clase.
+        int lonj;
+        int width;
+    public: // Metodos de la clase.
+        //Inicializando Constructor.
+        void rectangulo(int, int);
+        void perimeter();
+        void area(); 
+};
+
+int main()
+{
+
+
+
+    return 0;
+}
