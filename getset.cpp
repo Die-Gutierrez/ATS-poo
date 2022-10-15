aed694c6115a27fa34ed3d1047ac3e03e92ba6f2
Die-Gutierrez <dgutierrez@unjbg.edu.pe>
2022-10-15 18:06:11 -0500

    we have implemented the method getters

--- getset.cpp
+++ getset.cpp
@@ -10,22 +10,35 @@ class Point
         int x,y;
     public:
         Point();
-        void setPoint(int);
+        //This is setters.
+        void setPoint(int, int);
+        //Thi is getters.
+        int getPointX();
+        int getPointY();
 };
 Point::Point()
 {
 }
-//
-void Point::setPoint(int _x)
+void Point::setPoint(int _x, int _y)
 {
     x = _x;
-    cout<<"The element "<<x<<" has been added successfully . . .";
+    y = _y;
+}
+int Point::getPointX()
+{
+    return x;
+}
+int Point::getPointY()
+{
+    return y;
 }
 int main()
 {
     Point point1;
 
-    point1.setPoint(15);
+    point1.setPoint(15,10);
+    cout<<point1.getPointX()<<endl;
+    cout<<point1.getPointY()<<endl;
 
     return 0;
 }
