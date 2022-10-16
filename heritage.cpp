a06d1a3a86280dad894cd6406a3b5ba390af6fb4
Die-Gutierrez <dgutierrez@unjbg.edu.pe>
2022-10-16 07:45:35 -0500

    Heritage-Poo

--- heritage.cpp
+++ heritage.cpp
@@ -5,30 +5,51 @@ using namespace std;
 
 class Person
 {
-    // 'proteced', mediante esta palabra hacemos que la clase padre pueda ser modificada por las 
-    // clases hijas, modificando asi los 'atributos'
-    protected:
+    private:
         string name;
         int age;
     public:
         Person(string, int);
         void mostrar();
 };
+// Student heritage all of the class Person.
+class Student : public Person
+{
+    private:
+        string codeS;
+        int noteE;
+    public:
+        Student(string, int, string, int);
+        void show();
+};
 Person::Person(string _name, int _age)
 {
     name = _name;
     age = _age;
+}
+// We denote that the Person class already has values for 'Person(_name,_age)'
+Student::Student(string _name, int _age, string _codeS, int _noteE) : Person(_name, _age)
+{
+    codeS = _codeS;
+    noteE = _noteE;
 }
 void Person::mostrar()
 {
     cout<<"Name: "<<name<<endl;
     cout<<"Age: "<<age<<endl;
 }
+void Student::show()
+{
+    mostrar(); // Why my class student was heritage of u Daddy the methods.
+    cout<<"The student code: "<<codeS<<endl;
+    cout<<"Student's final mark: "<<noteE<<endl;
+}
 int main()
 {   
-    Person p1("Diego", 21);
-
+    Person p1("Diego", 22);
     p1.mostrar();
+    Student p2("Diego", 21, "2019-119046", 16);
+    p2.show();
 
     return 0;
 }
