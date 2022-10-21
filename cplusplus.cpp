f6d806bd5e48c446e9378540b9e02cfe0680533e
Die-Gutierrez <dgutierrez@unjbg.edu.pe>
2022-10-21 17:50:04 -0500

    2da version - agregamos otro objeto para demostrar que son diferentes

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -16,10 +16,12 @@ void Rectangulo::set_value(int a, int b)
 }
 int main()
 {
-    Rectangulo rea1;
+    Rectangulo rea1, rea2;
     // Aqui el objeto ya tiene dentro de sus miembros de dato los valores (4, 5).
     rea1.set_value(4,5);
-    cout<<"area : "<<rea1.area()<<endl;
+    rea2.set_value(10,6);
+    cout<<"area de rea1 : "<<rea1.area()<<endl;
+    cout<<"area de rea2 : "<<rea2.area()<<endl;
 
     return 0;
 }
